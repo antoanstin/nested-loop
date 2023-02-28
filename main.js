27bbcc405763301b0c870010d5eb7fc5377a12dc
antoanstin <antoanstin@mail.com>
2023-02-28 11:00:54 +0530

    ko

---
+++ main.js
@@ -0,0 +1,17 @@
+function series(){
+		for(i=1;i<=5;i++)
+		{ for(j=1;j<=i;j++)
+				document.write(i);
+		document.write("<br>");}
+
+		}
+		function hai(){
+			for(i=5;i>=1;i--)
+			{ for(j=1;j<=i;j++)
+				document.write(j);
+		document.write("<br>");}}
+			
+			
+			series();
+			
+			
